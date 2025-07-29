9f365beab29797448d2ae0c7bbc4088fad06c921
harshit <harshitmestry619@gmail.com>
2025-07-29 18:43:56 +0530

    navigation

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -2,7 +2,7 @@ const Navigation = () => {
   return (
     <div className="nav flex flex-row justify-between items-center">
       <div className="flex flex-row justify-start gap-1">
-        <div className="bg-grey p-2 rounded-full ">
+        <div className="bg-grey p-2 rounded-full hover:cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -13,7 +13,7 @@ const Navigation = () => {
             <path d="M240-160q-33 0-56.5-23.5T160-240q0-33 23.5-56.5T240-320q33 0 56.5 23.5T320-240q0 33-23.5 56.5T240-160Zm240 0q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm240 0q-33 0-56.5-23.5T640-240q0-33 23.5-56.5T720-320q33 0 56.5 23.5T800-240q0 33-23.5 56.5T720-160ZM240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400ZM240-640q-33 0-56.5-23.5T160-720q0-33 23.5-56.5T240-800q33 0 56.5 23.5T320-720q0 33-23.5 56.5T240-640Zm240 0q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Zm240 0q-33 0-56.5-23.5T640-720q0-33 23.5-56.5T720-800q33 0 56.5 23.5T800-720q0 33-23.5 56.5T720-640Z" />
           </svg>
         </div>
-        <div className="bg-grey p-2 rounded-full">
+        <div className="bg-grey p-2 rounded-full hover:cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -25,7 +25,7 @@ const Navigation = () => {
           </svg>
         </div>
         <div className="flex flex-row bg-grey p-2 rounded-full">
-          <div className="flex flex-row mr-1">
+          <div className="flex flex-row mr-1 ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
@@ -48,12 +48,13 @@ const Navigation = () => {
             viewBox="0 -960 960 960"
             width="24px"
             fill="oklch(0.61 0 0)"
+            className="hover:cursor-pointer"
           >
             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
           </svg>
           <input
             placeholder="Search Anything here"
-            className="px-2 text-xs flex-1 bg-transparent outline-none"
+            className="px-2 text-xs flex-1 bg-transparent outline-none hover:cursor-pointer"
           ></input>
         </div>
         <svg
@@ -62,13 +63,14 @@ const Navigation = () => {
           viewBox="0 -960 960 960"
           width="24px"
           fill="oklch(0.61 0 0)"
+          className="hover:cursor-pointer"
         >
           <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
         </svg>
       </div>
 
       <div className="flex flex-row justify-end gap-1">
-        <div className="notifs bg-grey p-2 rounded-full">
+        <div className="notifs bg-grey p-2 rounded-full hover:cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -79,7 +81,7 @@ const Navigation = () => {
             <path d="M80-560q0-100 44.5-183.5T244-882l47 64q-60 44-95.5 111T160-560H80Zm720 0q0-80-35.5-147T669-818l47-64q75 55 119.5 138.5T880-560h-80ZM160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
           </svg>
         </div>
-        <div className="help bg-grey p-2 rounded-full">
+        <div className="help bg-grey p-2 rounded-full hover:cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -90,7 +92,7 @@ const Navigation = () => {
             <path d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
           </svg>
         </div>
-        <div className="profile bg-grey p-2 rounded-full">
+        <div className="profile bg-grey p-2 rounded-full hover:cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
